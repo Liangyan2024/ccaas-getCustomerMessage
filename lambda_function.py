4419438e99214074aa0cbb72d340bc811ced6cc3
Liangyan2024 <liang.yan@telusinternational.com>
2025-05-01 23:59:14 -0400

    test2

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Customer message retrieved.' /*dddd 
-        
+        'body': 'Customer message retrieved.' 
+
     }
