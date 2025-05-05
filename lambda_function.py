4901946f9f688d268ae8ffbf90475d667f997926
Liangyan2024 <liang.yan@telusinternational.com>
2025-05-05 00:23:47 -0400

    updates of May 04

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,31 @@
 import json
 import boto3
 
-def lambda_handler(event, context):
-    print("Received event:", event)
 
-    return
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('ccaas-customerMessage')
+
+
+def lambda_handler(event, context):
+
+   print("Received event:", event)
+
+   attrs = event['Details']['ContactData']['Attributes']
+   language = attrs.get('language', 'en').lower()
+   message_id = attrs.get('messageID', '1')
+
+
+   resp = table.get_item(key = {'messageID': message_id})
+   item = resp.get('Item', {})
+
+   if language == 'en':
+      message_text = item.get('EnglishText', 'no English message found')
+   else:
+      message_text = item.get('FrenchText', 'no french message found')   
+
+      
+
+   return {
+      'message_test': message_text
+   }
+
